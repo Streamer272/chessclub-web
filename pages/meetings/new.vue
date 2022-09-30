d0f544d5bffca3d8b0bc03163aaa1cb513c91800
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-09-30 11:51:28 -0700

    🚧️ Adds meetings/new rooute

---
+++ pages/meetings/new.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page centered column">
+        <form class="form" @submit="submit">
+            <button type="submit">Create</button>
+        </form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts" setup>
+// `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+const submit = (event) => {
+    event.preventDefault()
+}
+</script>
